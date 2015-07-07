46285093a64353a5e22ab81e9e293bcde540ca9b
Caroline <animals4all@gmail.com>
2015-07-07 16:56:40 -0400

    Extra Credit

--- wordcount.py
+++ wordcount.py
@@ -1,20 +1,49 @@
 from sys import argv
 
-#script, filename = argv
-filename = open("twain.txt")
-contents = filename.read()
+PUNCTUATION = list("~`!@#$%^&*()_+={}[]|\:;\"'<>?,./")
 
-# Remove newline characters
-wordStr = contents.replace("\n", " ")
-# Turn from string into list of words
-wordList = wordStr.split(" ")
+def main():
+	# Get the name of the script and the name of the file from the command line
+	filename = "t.txt"
+	contents = open(filename)
 
-wordDict = {}
-for word in wordList:
-	if word in wordDict:
-		wordDict[word] += 1
-	else:
-		wordDict[word] = 1
+	# Remove newline characters and seperator hypens from the file contents, and split the
+	# contents into a list of all the words in it
+	wordList = contents.replace("\n", " ").replace(" - ", " ").replace("--", " ").split(" ")
 
-for key, value in wordDict.iteritems():
-	print key + " " + str(value)
+	# Create a dictionary with each of the words and the number of times that word occurs
+	wordDict = {}
+	for word in wordList:
+		# Remove any punctuation from the file contents that doesn't act as a seperator for
+		# two different words
+		for char in PUNCTUATION:
+			word = word.replace(char, "")
+
+		# Don't count words that are blank strings, such as empty lines in a file
+		if word != "":
+			word = word.lower()
+
+			if word in wordDict:
+				wordDict[word] += 1
+			else:
+				wordDict[word] = 1
+
+	# Create a dictionary with the numbers of word occurences as the keys and the words that
+	# occur that many times as the values
+	arrangedItems = {}
+	for key, value in wordDict.iteritems():
+		if value in arrangedItems:
+			arrangedItems[value].append(key)
+		else:
+			arrangedItems[value] = [key]
+
+	# Print the words and their occurences in order of frequency, with words having the same
+	# frequency being sorted alphabetically
+	for num in sorted(arrangedItems):
+		words = arrangedItems[num]
+		words.sort()
+		for word in words:
+			print word + " " + str(num)
+
+if __name__ == "__main__":
+	main()
